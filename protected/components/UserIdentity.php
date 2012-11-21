aaca4d61faaf2d2025385f1fd66189b67dc616b1
Guilherme Severo <grsevero@gmail.com>
2012-11-21 00:18:06 -0200

    LoginForm + UserIdentity

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -7,10 +7,13 @@
  */
 class UserIdentity extends CUserIdentity
 {
+
+	private $_id = null;
+	private $_email = null;
 	
 	/**
 	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
+	 * The example implementation makes sure if the email and password
 	 * are both 'demo'.
 	 * In practical applications, this should be changed to authenticate
 	 * against some persistent user identity storage (e.g. database).
@@ -19,16 +22,78 @@ class UserIdentity extends CUserIdentity
 	public function authenticate()
 	{
 		$users=array(
-			// username => password
+			// email => password
 			'demo'=>'demo',
 			'admin'=>'admin',
 		);
-		if(!isset($users[$this->username]))
+		if(!isset($users[$this->email]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if($users[$this->username]!==$this->password)
+		else if($users[$this->email]!==$this->password)
 			$this->errorCode=self::ERROR_PASSWORD_INVALID;
 		else
 			$this->errorCode=self::ERROR_NONE;
 		return !$this->errorCode;
 	}
+
+
+	/**
+	 * 
+	 * Autentica o usuário conforme o banco de dados
+	 * @return boolean
+	 * @property integer $id
+ * @property string $name
+ * @property string $password
+ * @property integer $age
+ * @property string $gender
+ * @property string $accessToken
+ * @property string $photo
+	 */
+	public function auth()
+	{		
+			
+			//Carrega atributos do usuário
+	   		$user = new User();
+	   		$user = $user->findByUserName($this->email);
+	   					
+		
+		if($user === null) //Verifica se o email é válido
+			$this->errorCode=self::ERROR_USERNAME_INVALID;
+		else if($user->password !== md5($this->password)){ //Verifica se o pass é válido
+
+			    $this->errorCode=self::ERROR_PASSWORD_INVALID;
+	   			
+	   			$this->_name = $user->login;
+	   			$this->_id = $user->id;
+			}
+		else{
+				//Login foi bem sucedido
+	   			$this->errorCode=self::ERROR_NONE;
+	   			   			
+	   			//Atribui os atributos
+	   			$this->_name = $user->login;
+	   			$this->_id = $user->id;
+		}
+
+		return !$this->errorCode;
+	}
+
+	
+	/**
+	 * 
+	 * Retorna o nome do usuário
+	 * @return string $username
+	 */
+	
+	public function getName(){
+		return ($this->_name == null) ? $this->email : $this->_email;
+	}
+	/**
+	 * 
+	 * Retorna o nome do usuário
+	 * @return string $id
+	 */
+	public function getId(){
+		return ($this->_id != null) ? $this->_id : $this->email;	
+	}
+
 }
